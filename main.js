d03ed97d0cf5ce4ef7ff34f47d3ed8c10548e2d4
Asadbek-Erkinov <asadbekeekinovg777@gmail.com>
2025-03-02 07:58:42 +0500

    done hero

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 /* menu show */ 
-
 const showMenu = (toggleId, navId) => {
   const toggle = document.getElementById(toggleId),
   nav = document.getElementById(navId)
-
+  
   if(toggle && nav) {
     toggle.addEventListener("click", ()=>{
       nav.classList.toggle("show")
@@ -20,7 +19,7 @@ function linkAction() {
   // Active link 
   navLink.forEach(n => n.classList.remove("active"))
   this.classList.add("active")
-
+  
   // Remove menu mobile
   const navMenu = document.getElementById("nav-menu")
   navMenu.classList.remove("show")
